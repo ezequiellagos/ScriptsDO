724271ac43c7b6a00559bebc522a10e93844b3fc
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2019-04-13 22:52:16 -0400

    Update: Reorganización de archivos

--- create.sh
+++ create.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Descripción: Permite crear un nuevo script vacio añadiendole los permisos de ejecución
+
 printf "Ingrese nombre del script: "
 read file
 
 cat > $file << EOF
 #!/bin/bash
+
+
 
 EOF
 
